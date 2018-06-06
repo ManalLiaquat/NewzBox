914e4e89e4ef9c5b241d58c2a50514f810db492b
ManalLiaquat <m.manalliaquat@gmail.com>
2018-06-07 00:55:03 +0500

    This is a service worker file to run the app offline

---
+++ serviceworker.js
@@ -0,0 +1,66 @@
+const cacheName = 'news-box';
+const staticAssets = [
+    './',
+    './css/style.css',
+    './index.html',
+    './api2.html',
+    './api3.html',
+    './api4.html',
+    './api5.html',
+    './js/app.js',
+    './js/api2.js',
+    './js/api3.js',
+    './js/api4.js',
+    './js/api5.js'
+]
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(staticAssets);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(req))
+    } else {
+        event.respondWith(networkFirst(req))
+    }
+})
+
+async function cacheFirst(req) {
+    const cacheResponse = await caches.match(req);
+    return cacheResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open(cacheName);
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone())
+        return res
+    } catch (error) {
+        return await cache.match(req)
+    }
+}
